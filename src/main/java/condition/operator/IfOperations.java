c4891576b15d95682bc0319765a40f52ae8731ff
jaindj2411 <78894897+jaindj2411@users.noreply.github.com>
2021-06-18 18:30:14 +0530

    Update IfOperations.java

--- src/main/java/condition/operator/IfOperations.java
+++ src/main/java/condition/operator/IfOperations.java
@@ -4,7 +4,7 @@ public class IfOperations {
 
     public static void checkNumberIsNegative(int num){
         if(num<0){
-            System.out.println("Number is negative");
+            System.out.println("number_is_negative");
         }
         else {
             System.out.println("Number is positive");
