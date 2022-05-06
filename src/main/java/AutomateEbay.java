ce40172814d31c80e3003e79f55299d000a3a6fe
Anku jain <jain.20a@gmail.com>
2022-05-06 08:13:18 +0530

    add selenium dependencies

---
+++ src/main/java/AutomateEbay.java
@@ -0,0 +1,48 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.Set;
+
+public class AutomateEbay {
+    public static void main(String[] args) {
+        String ebayUrl = "https://www.ebay.com/";
+        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe" );
+
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get(ebayUrl);
+
+        //XPaths
+        String allCategoryDrpDwn = "//select[@aria-label=\"Select a category for search\"]";
+        String searchBtn = "//input[@type=\"submit\"]";
+        String orignalWebLink = "//a[contains(text(),\"originalfables.com\")]/parent::div/following-sibling::div/a";
+
+        WebElement drpDownElement = webDriver.findElement(By.id(allCategoryDrpDwn));
+
+        Select dropDown = new Select(drpDownElement);
+        dropDown.selectByValue("Clothing, Shoes & Accessories");
+
+        WebElement searchBtnElement = webDriver.findElement(By.id(searchBtn));
+        searchBtnElement.click();
+
+        if(searchBtnElement.isDisplayed()){
+
+        }
+
+        WebElement orignalWebLinkElement = webDriver.findElement(By.id(orignalWebLink));
+        orignalWebLinkElement.click();
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
